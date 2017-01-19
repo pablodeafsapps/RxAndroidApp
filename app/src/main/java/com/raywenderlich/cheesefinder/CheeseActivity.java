307b054aafe53079d1a1479ffdc55de8ed8e5763
pablodeafsapps <pablodeafsapps@gmail.com>
2017-01-19 20:45:42 +0100

    The two Observable streams have been merged in one. Furthermore, 'debounce' method is added to avoid succesive queries for events very close in time. Finally, the app unsubscribes from any Observable stream in onStop.

--- app/src/main/java/com/raywenderlich/cheesefinder/CheeseActivity.java
+++ app/src/main/java/com/raywenderlich/cheesefinder/CheeseActivity.java
@@ -24,65 +24,50 @@ package com.raywenderlich.cheesefinder;
 
 
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Cancellable;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 
 public class CheeseActivity extends BaseSearchActivity {
 
     private static final String TAG = CheeseActivity.class.getSimpleName();
 
+    private Disposable mDisposable;
+
+    /*
+     * The 'onStart' method is an ideal place to subscribe to any Observable
+     */
     @Override
     protected void onStart() {
         super.onStart();   // It is always important to let the parent perform first!
 
-        /*
-         * The 'onStart' method is an ideal place to subscribe to any Observable
-         */
         // Subscription to Button-click events
-        this.createButtonClickObservable()
+        Observable<String> buttonClickStream = this.createButtonClickObservable();
+        // Subscription to EditText-change events
+        Observable<String> textChangeStream = this.createTextChangeObservable();
+        // 'merge' agglutinates both Observable streams
+        Observable<String> searchTextObservable = Observable.merge(buttonClickStream, textChangeStream);
+        // Once instantiated and merged, the Observable performance is configured (threads, map, etc.)
+        this.mDisposable = searchTextObservable
                 .observeOn(AndroidSchedulers.mainThread())   // Ensures that the next operator in chain will be run on the main thread
                 .doOnNext(new Consumer<String>() {   // Called every time a new item is emitted
                     @Override
                     public void accept(String s) throws Exception {
-                        CheeseActivity.super.showProgressBar();
-                    }
-                })
-                .observeOn(Schedulers.io())   // The next operator should be called on the I/O thread
-                .map(new Function<String, List<String>>() {
-                    // For each search query, a list of results is returned
-                    @Override
-                    public List<String> apply(String query) throws Exception {
-                        return CheeseActivity.super.mCheeseSearchEngine.search(query);
-                    }
-                })
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<List<String>>() {
-                    // 'accept' will be called when the observable emits an item
-                    @Override
-                    public void accept(List<String> result) throws Exception {
-                        CheeseActivity.super.hideProgressBar();   // Hides the progress bar just before displaying a result
-                        CheeseActivity.super.showResult(result);
-                    }
-                });
-        // Subscription to EditText-change events
-        this.createTextChangeObservable()
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnNext(new Consumer<String>() {   // Called every time a new item is emitted
-                    @Override
-                    public void accept(String string) throws Exception {
-
                         CheeseActivity.super.showProgressBar();
                     }
                 })
@@ -135,11 +120,11 @@ public class CheeseActivity extends BaseSearchActivity {
     // Declares a method that returns an observable that will emit strings when an EditText changes
     private Observable<String> createTextChangeObservable() {
 
-        return Observable.create(new ObservableOnSubscribe<String>() {
+        Observable<String> textChangeObservable = Observable.create(new ObservableOnSubscribe<String>() {
             @Override
             public void subscribe(final ObservableEmitter<String> emitter) throws Exception {
 
-                CheeseActivity.super.mQueryEditText.addTextChangedListener(new TextWatcher() {
+                final TextWatcher watcher = new TextWatcher() {
                     // When the EditText content changes, 'onNext' on the emitter to send a String
                     @Override
                     public void beforeTextChanged(CharSequence string, int start, int count, int after) {
@@ -147,21 +132,46 @@ public class CheeseActivity extends BaseSearchActivity {
 
                     @Override
                     public void onTextChanged(CharSequence string, int start, int before, int count) {
+                        Log.d(CheeseActivity.TAG, "In onTextChanged");
+                        emitter.onNext(string.toString());
                     }
 
                     @Override
-                    public void afterTextChanged(Editable editable) {
-                        emitter.onNext(editable.toString());
+                    public void afterTextChanged(Editable s) {
                     }
-                });
+                };
+
+                CheeseActivity.super.mQueryEditText.addTextChangedListener(watcher);
 
                 emitter.setCancellable(new Cancellable() {
                     @Override
                     public void cancel() throws Exception {
-                        CheeseActivity.super.mQueryEditText.addTextChangedListener(null);
+                        CheeseActivity.super.mQueryEditText.removeTextChangedListener(watcher);
                     }
                 });
             }
         });
+
+        // The Ovservable instance is returned as long as the String length is longer than 2 characters
+        return textChangeObservable
+                .filter(new Predicate<String>() {   // 'filter' only allows to pass those Observable emissions which satisfy the 'test' Predicate
+                    @Override
+                    public boolean test(String query) throws Exception {
+                        return query.trim().length() > 2;
+                    }
+                })
+                .debounce(1000, TimeUnit.MILLISECONDS);   // 'debounce' filters out those events which happen within a particular time lapse
+    }
+
+    /*
+     * The 'onStop' method is an ideal place to unsubscribe from any Observable
+     */
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (!this.mDisposable.isDisposed()) {
+            this.mDisposable.dispose();
+        }
     }
 }
